e63bc5330a69098a764e81b9d730513786b14274
Muhammad Huzaifah <mhuzaifah989@gmail.com>
2024-08-27 00:30:58 -0400

    Project Structure
    Setup React app for Frontend
    Setup Spring Boot app for Backend
    Connected with PostgreSQL
    Added Data Scraping Files

---
+++ Backend/src/main/java/com/ifooty/IFootyApplication.java
@@ -0,0 +1,13 @@
+package com.ifooty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IFootyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IFootyApplication.class, args);
+	}
+
+}
