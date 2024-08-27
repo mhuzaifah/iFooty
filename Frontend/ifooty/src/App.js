2025dbba328840642f98616b3114933fc687196e
Muhammad Huzaifah <mhuzaifah989@gmail.com>
2024-08-27 01:04:35 -0400

    Frontend Updates

--- Frontend/ifooty/src/App.js
+++ Frontend/ifooty/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from 'react-router-dom';
+import VerticalNavBar from "./components/NavBar";
+import {Container} from "react-bootstrap";
+import MyTeam from "./components/MyTeam";
+import Landing from "./components/Landing";
+import Teams from "./components/Teams";
+import Players from "./components/Players";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <VerticalNavBar />
+          <Container fluid>
+              <Routes>
+                  <Route index element={<Landing />} />
+                  <Route path="/myteam" element={<MyTeam />} />
+                  <Route path="/players" element={<Players />} />
+                  <Route path="/teams" element={<Teams />} />
+              </Routes>
+          </Container>
+      </div>
   );
 }
 
