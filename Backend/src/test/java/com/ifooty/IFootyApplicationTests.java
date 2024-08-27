3c6e142d4852b5129c897fb0bb1f8c1739665134
Muhammad Huzaifah <mhuzaifah989@gmail.com>
2024-08-27 00:30:58 -0400

    Project Structure
    Setup React app for Frontend
    Setup Spring Boot app for Backend
    Connected with PostgreSQL
    Added Data Scraping Files

---
+++ Backend/src/test/java/com/ifooty/IFootyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifooty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IFootyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
